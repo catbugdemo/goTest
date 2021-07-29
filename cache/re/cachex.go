b6e00baea34959e724bd8643f39ccf5bc609a642
catbugdemo <2229587083@qq.com>
2021-07-29 20:04:07 +0800

    最近几天的学习成果

---
+++ cache/re/cachex.go
@@ -0,0 +1,10 @@
+package cache
+
+// 复习 go-cache
+// 今天，可以来学习写下 go-cache
+// 本项目代码全部内容可在 github.com/patrickmn/go-cache 中查询
+
+// 需要的前置知识
+//	1.什么是读锁，什么是写锁， sync.Mutex 和 sync.RMutex 区别
+//	2.go runtime 协程参考
+//	3.
